d016547938d9396f757fbb3f7fa16ab7d2674948
adi0210 <78998333+adi0210@users.noreply.github.com>
2021-06-22 12:53:56 +0530

    first upload

---
+++ SQL/flight path.sql
@@ -0,0 +1,34 @@
+CREATE TABLE flights
+
+(
+    
+	
+	id SERIAL PRIMARY KEY,
+
+    
+	origin VARCHAR NOT NULL,
+
+    
+	destination VARCHAR NOT NULL,
+
+    
+	duration INTEGER NOT NULL,
+
+
+);
+
+INSERT INTO flights
+
+(
+
+	origin, destination, duration
+
+);
+
+VALUES
+
+(
+    
+	delhi, kolkata, 120
+
+);
